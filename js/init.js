92d1ab29a58963f9a52e24462dfbac973303fa46
fanian <rfanian@gmail.com>
2014-11-29 16:42:23 +0300

    add header and nav and intro sections

---
+++ js/init.js
@@ -0,0 +1,7 @@
+head.load("js/lib/jquery.js",
+					"js/common.js",
+                    "js/lib/jquery.validate.js",
+                    "js/lib/jquery.fancybox.pack.js",
+                    "js/lib/jquery.form.js",
+                    "js/lib/bootstrap.js"
+                     );
